b8e4b2d24c22cfab73b21ea0edea105e4d385c9b
Kim <113918212+kimlukasmyrvold@users.noreply.github.com>
2023-02-07 11:23:01 +0100

    fixed linking

--- javascript/index.js
+++ javascript/index.js
@@ -12,25 +12,25 @@ document.addEventListener('keydown', function (event) {
 function playSound(key) {
     switch (key) {
         case 'w':
-            new Audio('../sounds/tom-1.mp3').play();
+            new Audio('./sounds/tom-1.mp3').play();
             break;
         case 'a':
-            new Audio('../sounds/tom-2.mp3').play();
+            new Audio('./sounds/tom-2.mp3').play();
             break;
         case 's':
-            new Audio('../sounds/tom-3.mp3').play();
+            new Audio('./sounds/tom-3.mp3').play();
             break;
         case 'd':
-            new Audio('../sounds/tom-4.mp3').play();
+            new Audio('./sounds/tom-4.mp3').play();
             break;
         case 'j':
-            new Audio('../sounds/snare.mp3').play();
+            new Audio('./sounds/snare.mp3').play();
             break;
         case 'k':
-            new Audio('../sounds/crash.mp3').play();
+            new Audio('./sounds/crash.mp3').play();
             break;
         case 'l':
-            new Audio('../sounds/kick-bass.mp3').play();
+            new Audio('./sounds/kick-bass.mp3').play();
             break;
         default:
             break;
